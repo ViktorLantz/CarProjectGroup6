ffe5d6e88e972fe10fa7d8fa0552d0edf8bd53de
Oscar <oscar.bergstrom@icloud.com>
2016-05-17 15:41:40 +0200

    Parker for car commented and done.

--- Parker.cpp
+++ Parker.cpp
@@ -1,7 +1,9 @@
-/**
+/*
+				        MAIN PARKER FOR CAR
+                            HARDCODED
  * Parker is an example application to demonstrate how to 
- *         generate driving commands from an application realized
- *         with OpenDaVINCI
+ * generate driving commands from an application realized
+ * with OpenDaVINCI
  * Copyright (C) 2012 - 2015 Christian Berger
  *
  * This program is free software; you can redistribute it and/or
@@ -21,21 +23,15 @@
 
 #include <cmath>
 #include <iostream>
-
-
 #include <cstdio>
 #include "opendavinci/odcore/data/Container.h"
 #include "opendavinci/GeneratedHeaders_OpenDaVINCI.h"
 #include "opendavinci/odcore/io/conference/ContainerConference.h"
- #include "automotivedata/GeneratedHeaders_AutomotiveData.h"
-
-
-
+#include "automotivedata/GeneratedHeaders_AutomotiveData.h"
 #include "Parker.h"
 
 namespace automotive {
     namespace miniature {
-
         using namespace std;
         using namespace odcore::base;
         using namespace odcore::base::module;
@@ -56,27 +52,17 @@ namespace automotive {
             // This method will be call automatically _after_ return from body().
         }
  
-        vector<double> Parker::getFoundGaps() const {
-        		return m_foundGaps;
-        }
-		
-
-
         // This method will do the main data processing job.
         odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode Parker::body() {
-        	double distanceOld = 0;
-        	double absPathStart = 0;
-        	double absPathEnd = 0;
-
-        	int stageMoving = 0;
-        	int stageMeasuring = 0;
-        	bool parkingSpotFound = false;
-			//double back_distance = 0;
-
+        int stageMoving = 0;
+		int sideSensorValueF = 0;         // Side-front IR-sensor
+        int sideSensorValueB = 0;         // Side-back IR-sensor
+        int sideSensorGapCounterF = 0;    // Counter for side-front IR-sensor
+        int sideSensorGapCounterB = 0;    // Counter for side-back IR-sensor
+        bool parkingSpotFound = false;    // When true - stops "looking" for a parking spot.
+ 
+        	while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
 			
-
-            while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
-
                 // Get most recent vehicle data:
                 Container containerVehicleData = getKeyValueDataStore().get(automotive::VehicleData::ID());
                 VehicleData vd = containerVehicleData.getData<VehicleData> ();
@@ -85,154 +71,172 @@ namespace automotive {
                 Container containerSensorBoardData = getKeyValueDataStore().get(automotive::miniature::SensorBoardData::ID());
                 SensorBoardData sbd = containerSensorBoardData.getData<SensorBoardData> ();
 
-
-                //	back_distance = sbd.getValueForKey_MapOfDistances(1);
-
-                /*
-                if((back_distance > 1.5) && (back_distance < 2)){
-                			vc.setSpeed(0);
-                			cout << "\n STOP THE CAR \n";
-                			stageMeasuring++;
-                		}
-                */
-               
+				sideSensorValueF = sbd.getValueForKey_MapOfDistances(2); // Side-front IR-sensor 
+                sideSensorValueB = sbd.getValueForKey_MapOfDistances(0); // Side-back IR-sensor
+                
                 // Create vehicle control data.
                 VehicleControl vc;
-                // Moving state machine.
-                if(stageMoving == 0) {
-                	//Put the speed to 1 and listen to the most recent steering data
-                	vc.setSpeed(1);
-                	vc.setSteeringWheelAngle(0);
+
+	            if(parkingSpotFound == false){
+    	            if(sideSensorValueF > 50){      // Adds to counter when the vehicle detects gap on the side      
+        	            sideSensorGapCounterF++;
+            	    }
+            	}
+
+                if(sideSensorValueF > 50){      // Adds to counter when the vehicle detects a gap on the side      
+                    sideSensorGapCounterF++;
                 }
-                if((stageMoving > 0) && (stageMoving < 10)) {
-                	//Move slowly forward
+
+                if(sideSensorValueB > 50){      // -- || --
+                    sideSensorGapCounterB++;
+                }
+                
+                if(parkingSpotFound == false){ 
+                     // Check if the car should start parking
+                	if ((sideSensorGapCounterF > 5) && (sideSensorGapCounterB > 5) && sideSensorValueF < 30 && sideSensorValueB < 30 && sideSensorValueF > 5 && sideSensorValueB > 5){
+                    	stageMoving++;
+                    	parkingSpotFound = true;
+                  	}
+                }
+
+           /* 	START OF MOVING STATE MACHINE with hardcoded values.
+            * 	Every stage has a "resting" stage in between them, this
+            * 	is coded like this to give the car time to readjust the
+            * 	wheels and to maintain the same behavior as good as possible 
+            * 	regardless of the battery level of the car. The first stage is
+            * 	the "looking for a spot" stage and differentiates in the way
+            * 	it moves on to the next stage compaired to the other stages. 
+			*	vc.setSteeringWheelAngle(1) = right
+			*	vc.setSteeringWheelAngle(-1) = left
+			*	vc.setSteeringWheelAngle(5) = neutral
+			*	vc.setSpeed(1) = foward
+			*	vc.setSpeed(2) = brake
+			*	vc.setSpeed(4) = neutral
+			*	vc.setSpeed(-1) = backwards	
+            */
+				
+				// Going forward
+                if(stageMoving == 0) {
                 	vc.setSpeed(1);
-                	vc.setSteeringWheelAngle(0);
+                	vc.setSteeringWheelAngle(5);
+                }
+				// Brake
+                if((stageMoving > 0) && (stageMoving < 2)) {
+                	vc.setSpeed(2);
+                	vc.setSteeringWheelAngle(5);
                 	stageMoving++;
                 }
-                if ((stageMoving >= 10) && (stageMoving < 15)) {
-                    // Stop when it has passed the gap
-                    vc.setSpeed(0);
-                    vc.setSteeringWheelAngle(0);
-                    stageMoving++;
+				// Neutral with neutral steering
+                if((stageMoving >= 2) && (stageMoving < 40)) {
+                	vc.setSpeed(4);
+                	vc.setSteeringWheelAngle(5);
+                	stageMoving++;
                 }
-                if ((stageMoving >= 15) && (stageMoving < 70)) {
-                    // Go backwards, steering wheel to the right to get the appropriate angle
+                // Stage when parking starts
+				// Neutral steering right 
+                if ((stageMoving >= 40) && (stageMoving < 60)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(1);
+                    stageMoving++;    
+                }
+				// Backwards steering right
+                if ((stageMoving >= 60) && (stageMoving < 80)) {
                     vc.setSpeed(-1);
-                    vc.setSteeringWheelAngle(25);
+                    vc.setSteeringWheelAngle(1);
                     stageMoving++;
                 }
-
-				if((stageMoving >= 70) && (stageMoving < 100)) {
-					// Turn left to straighten up while going backwards
-					vc.setSpeed(-1);
-					vc.setSteeringWheelAngle(-25);
-					stageMoving++;
-				}
-
-				if((stageMoving >= 100) && (stageMoving < 103)) {
-					// Stop
-					vc.setSpeed(0);
-					vc.setSteeringWheelAngle(-25);
-					stageMoving++;
-				}
-
-				if((stageMoving >= 103) && (stageMoving < 127)) {
-					// Turn right to straighten up while going forward
-					vc.setSpeed(1);
-					vc.setSteeringWheelAngle(25);
-					stageMoving++;
-				}
-
-				if((stageMoving >= 127) && (stageMoving < 137)){
-					// Go slightly foward to finalize the parking
-					vc.setSpeed(1);
-					vc.setSteeringWheelAngle(0);
-					stageMoving++;
-				}
-                if ((stageMoving >= 137) && (stageMoving < 147)) {
-                    // Stop.
-                    vc.setSpeed(0);
-                    vc.setSteeringWheelAngle(0);
+				// Neutral steering right
+                if ((stageMoving >= 80) && (stageMoving < 100)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(1);
                     stageMoving++;
                 }
-                if (stageMoving >= 147	) {
-                    // End component.
+				// Neutral steering right
+                if ((stageMoving >= 100) && (stageMoving < 110)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(1);
+                    stageMoving++;
+                }
+				// Neutral steering left
+                if ((stageMoving >= 110) && (stageMoving < 130)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(-1);
+                    stageMoving++;
+                }
+				// Neutral steering left
+                if ((stageMoving >= 130) && (stageMoving <150)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(-1);
+                    stageMoving++;
+                }
+				// Backwards steering left
+                if ((stageMoving >= 150) && (stageMoving < 170)) {
+                    vc.setSpeed(-1);
+                    vc.setSteeringWheelAngle(-1);
+                    stageMoving++;
+                }
+				// Neutral steering left
+                if ((stageMoving >= 170) && (stageMoving < 180)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(-1);
+                    stageMoving++;
+                }
+				// Neutral steering left
+                if ((stageMoving >= 180) && (stageMoving < 190)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(-1);
+                    stageMoving++;
+                }
+				// Neutral with neutral steering
+                if ((stageMoving >= 190) && (stageMoving < 210)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(5);
+                    stageMoving++;
+                }
+				// Neutral steering right
+                if ((stageMoving >= 210) && (stageMoving < 235)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(1);
+                    stageMoving++;
+                }
+				// Forward with neutral steering
+                if ((stageMoving >= 235) && (stageMoving < 250)) {
+                    vc.setSpeed(1);
+                    vc.setSteeringWheelAngle(5);
+                    stageMoving++;
+                }
+				// Neutral steering right
+                if ((stageMoving >= 250) && (stageMoving < 255)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(1);
+                    stageMoving++;
+                }
+				// Neutral with neutral steering
+                if ((stageMoving >= 255) && (stageMoving < 265)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(5);
+                    stageMoving++;
+                }
+				// Neutral with neutral steering
+                if ((stageMoving >= 265) && (stageMoving < 267)) {
+                    vc.setSpeed(4);
+                    vc.setSteeringWheelAngle(5);
+                    cout << "END COMPONENT" << endl;
                     break;
                 }
 
-                
-                //Measuring state machine.
-                switch (stageMeasuring) {
-                	case 0:
-                	{
-                			//Initialize measurement.
-                			if(parkingSpotFound == false){
-                				distanceOld = sbd.getValueForKey_MapOfDistances(2);
-                				stageMeasuring++;
-                				//cout << "\n			Entered CASE 0";
-                			}
-                	}
-                	break;
-               		case 1:
-           				{
-           					//cout << "\n			Entered CASE 1";
-               				// Checking the distance sequece +, -.
+            //  END OF STATE MACHINE
 
+               	// Create container for finally sending the data.
+   			    Container c(vc);
 
-               				if(parkingSpotFound == false){
-               					if ((distanceOld > 0) && (sbd.getValueForKey_MapOfDistances(2) < 0)) {
-               						//Found distance sequence -, +.
-               						stageMeasuring = 2;
-               						absPathStart = vd.getAbsTraveledPath();
-               						//cout << "			Entered CASE 1, IF-1";
-               					}
-               				}
-               				distanceOld = sbd.getValueForKey_MapOfDistances(2);
-               			}
-               		break;
-               		case 2:
-               			{
-               				//Ckecking for distance sequence -, +.
-               				if(parkingSpotFound == false){
-
-               				if ((distanceOld < 0) && (sbd.getValueForKey_MapOfDistances(2) > 0)) {
-               					//Found distance sequence -, +.
-               					stageMeasuring = 1;
-               					absPathEnd = vd.getAbsTraveledPath();
-               					//cout << "\n			Entered CASE 2, IF-1";
-               					
-               					
-               					const double GAP_SIZE = (absPathEnd - absPathStart);
-               					cerr << "\nSize = " <<GAP_SIZE << endl;
-               					m_foundGaps.push_back(GAP_SIZE);
-
-               					if((stageMoving < 1) && (GAP_SIZE > 4)) {
-               						stageMoving = 1;
-               						parkingSpotFound = true;
-               						cout << "\n Found Parking Spot \n";
-               					}
-               				}	
-               				
-               				}
-               				distanceOld = sbd.getValueForKey_MapOfDistances(2);	
-               			}
-               		break;
-               	}
-                // You can also turn on or off various lights:
-                vc.setBrakeLights(false);
-                vc.setFlashingLightsLeft(false);
-                vc.setFlashingLightsRight(true);
-
-                // Create container for finally sending the data.
-                Container c(vc);
-                // Send container.
+   				// Send container.
                 getConference().send(c);
             }
 
             return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
-        }
+        } //automotive::miniature::parker::src
 
-    }
+    } //automotive::miniature::parker
 } // automotive::miniature
 
